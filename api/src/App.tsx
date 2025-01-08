ec3c47dd498f2642042cdf3b7189e7d652bb8368
DUDE <nisbis194@gmail.com>
2025-01-08 21:28:15 +0545

    installed css and learnt about css prettier

--- api/src/App.tsx
+++ api/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function App() {
   const [data, setData] = useState<string | null>("");
+
   function api() {
     fetch("https://catfact.ninja/fact")
       .then((response) => response.json())
@@ -9,10 +10,17 @@ export default function App() {
         setData(a.fact);
       });
   }
+
   return (
     <div>
-      <button onClick={api}>Generate Cat Fact</button>
-      <p>{data}</p>
+      <button
+        onClick={api}
+        className="border border-blue-600 bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+      >
+        Generate Cat Fact
+      </button>
+
+      <p className="text- text-5xl text-red-500">{data}</p>
     </div>
   );
 }
